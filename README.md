b21b9d2f43f6d12c4bc0c70c1a6640b7fdb0d245
bulbipop <fierbenj@gmail.com>
2017-07-01 21:11:44 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 feedly-switcher
+
+A small script that adds a button to quickly switch between "Latest" and "Saved" pages.
---
+++ main.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Feedly-Switcher
+// @namespace    https://github.com/bulbipop/feedly-switcher
+// @version      0.1
+// @match        https://feedly.com/*
+// @run-at       document-idle
+// ==/UserScript==
+
+unsafeWindow.switcher = function() {
+    let target = document.querySelector('#myButton')
+    if (target.innerText == 'SAVED') {
+        document.querySelector('#savedtab_label').click();
+        target.innerText = 'LATEST'
+    } else {
+        document.querySelector('#latesttab_label').click();
+        target.innerText = 'SAVED'
+    }
+}
+
+function main() {
+    let button = '<button id="myButton" class="pro primary small" '
+    if (document.URL.substr(document.URL.lastIndexOf('/') + 1) == 'latest') {
+        button += 'onclick="switcher();">SAVED</button>'
+
+    } else {
+        button += 'onclick="switcher();">LATEST</button>'
+    }
+
+    let upgradeButton = document.querySelector('.pro.primary.small')
+    upgradeButton.insertAdjacentHTML('afterend', button)
+}
+
+setTimeout(main, 4000)
